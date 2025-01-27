a91a21644b71ee3eae1b72aa44a7959b47d73537
Virtual Jasper <jasper2virtual@hotmail.com>
2025-01-27 08:22:41 +0800

    Add Tailwind CSS configuration and implement main screen layout

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,13 @@
-<template></template>
+<template>
+    <div class="flex flex-col items-center justify-center w-screen min-h-screen bg-app-dark-navy">
+        <main-screen-container>
+            
+        </main-screen-container>
+    </div>
+</template>
+<script lang="ts" setup>
+    import MainScreenContainer from '@/components/main-screen-container.vue';
+    import NewGameMenu from '@/components/new-game-menu.vue';
+    const player1mark = ref('X');
+
+</script>
---
+++ src/components/main-screen-container.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex flex-col mx-auto w-full min-h-full 
+    app-tablet:min-h-fit app-tablet:max-w-[460px]">
+        <slot />
+    </div>
+</template>
