95ffe5e3d8f76be87c72078f20fa9071c2112c47
Virtual Jasper <jasper2virtual@hotmail.com>
2025-01-27 17:37:48 +0800

    Add game components and update App.vue for screen management

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-    <div class="flex flex-col items-center justify-center w-screen min-h-screen bg-app-dark-navy">
-        <main-screen-container>
-            
-        </main-screen-container>
-    </div>
+  <div class="flex flex-col items-center justify-center w-screen min-h-screen bg-app-dark-navy">
+    <main-screen-container>
+
+      <new-game-menu v-if="currentScreen === 'NewGameMenu'" />
+      <game-board v-if="currentScreen === 'GameBoard'" />
+
+    </main-screen-container>
+  </div>
 </template>
 <script lang="ts" setup>
-    import MainScreenContainer from '@/components/main-screen-container.vue';
-    import NewGameMenu from '@/components/new-game-menu.vue';
-    const player1mark = ref('X');
+import { ref } from 'vue';
+import MainScreenContainer from '@/components/main-screen-container.vue'
+import NewGameMenu from '@/components/new-game-menu.vue'
+import GameBoard from '@/components/game-board.vue'
+
+// Data
+const player1mark = ref('X');
+
+// State
+const currentScreen = ref<'NewGameMenu' | 'GameBoard'>('NewGameMenu');
+
 
 </script>
