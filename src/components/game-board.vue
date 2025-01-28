7ffd3c6c2710a2f4f5efabd8968cf5ef0854981f
Virtual Jasper <jasper2virtual@hotmail.com>
2025-01-28 18:04:58 +0800

    Refactor game board component for improved button styling and interaction feedback

--- src/components/game-board.vue
+++ src/components/game-board.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="game-board grid grid-cols-3 grid-rows-3 gap-4">
-        <button 
-        v-for="(cellState,index) in props.gameBoardState" :key="index"
-        @click="$emit('cellClick',index)"
-        :disabled="cellState !== null"
-        class="aspect-square
+        <button v-for="(cellState,index) in props.gameBoardState" :key="index" @click="$emit('cellClick',index)"
+            :disabled="cellState !== null" class="aspect-square
             bg-app-semi-dark-navy
             shadow-[0_4px_0_0_#10212A]
-            rounded-2xl flex items-center justify-center">
-        <img class="w-[45%] aspect-square"  v-if="cellState==='X'" src="@/images/icon-x.svg" alt="X" />
-        <img class="w-[45%] aspect-square" v-else-if="cellState==='O'" src="@/images/icon-o.svg" alt="O" />
+            rounded-2xl flex items-center justify-center" :class="{
+                'cursor-pointer': cellState === null,
+                'cursor-not-allowed': cellState !== null
+            }">
+            <img class="w-[45%] aspect-square" v-if="cellState==='X'" src="@/images/icon-x.svg" alt="X" />
+            <img class="w-[45%] aspect-square" v-else-if="cellState==='O'" src="@/images/icon-o.svg" alt="O" />
         </button>
     </div>
 </template>
